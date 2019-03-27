62ee6614fd75c6ac0d05a3d9bb9cb39072b7fdda
wad <wadwatsana@gmail.com>
2019-03-27 17:02:57 +0700

    update add remove 274/03/62(17.02)

---
+++ app/src/main/java/com/prospec/addremove/AddActivity.java
@@ -0,0 +1,32 @@
+package com.prospec.addremove;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class AddActivity extends AppCompatActivity {
+
+    private LinearLayout parentLinearLayout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        parentLinearLayout = (LinearLayout) findViewById(R.id.parent_linear_layout);
+    }
+
+    public void onAddField(View v) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View rowView = inflater.inflate(R.layout.field_add, null);
+        // Add the new row before the add field button.
+        parentLinearLayout.addView(rowView, parentLinearLayout.getChildCount() - 1);
+    }
+
+    public void onDelete(View v) {
+        parentLinearLayout.removeView((View) v.getParent());
+    }
+
+}
